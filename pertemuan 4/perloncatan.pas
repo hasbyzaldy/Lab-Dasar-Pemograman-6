943a8cfb9a8c4b0123bbf6c2273d7a06c30b4198
hasbyzaldy <hasbyzaldy25@gmail.com>
2025-10-26 17:19:23 +0700

    Perulangan

---
+++ pertemuan 4/perloncatan.pas
@@ -0,0 +1,14 @@
+uses crt;
+label a, b, c, d, e;
+begin
+clrscr;
+    a:writeln('hello world');
+    goto b;
+    d:writeln('Fasilkom-TI');
+    goto e;
+    c:writeln('ilmu komputer');
+    goto d;
+    b:writeln('IKLC');
+    goto c;
+    e:
+end.
---
+++ pertemuan 4/perulangan.pas
@@ -0,0 +1,31 @@
+uses crt;
+
+var
+    i, j : integer;
+    password : string;
+begin
+clrscr;
+    // for i := 1 to 5 do
+    //     writeln('hello world');
+
+    // for i := 1 to 5 do 
+    //     begin
+    //         for j := 1 to i do
+    //             write('* ');
+    //         writeln;
+    //     end;
+
+    // i := 1;
+    
+    // while i <= 10 do 
+    //     begin 
+    //         if i mod 2 = 0 then
+    //             write(i, ' ');
+    //         i := i + 1;
+    //     end;
+
+    repeat
+        write('masukkan password anda : '); readln(password);
+    until (password = '12345');
+    writeln('berhasil login');
+end.
